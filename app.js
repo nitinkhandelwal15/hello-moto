4eee57d2365a08e897db405bd5c4f02a56ac4204
Nitin Khandelwal <khandelwal.nitin.1505@gmail.com>
2024-09-28 19:02:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading3 = React.createElement("div", { id: "p1"},
+        [
+                React.createElement("div",
+                { id: "c1" },
+                        [
+                                React.createElement("h1", {}, " this is basically div > (div > h1*2)*2"), 
+                                React.createElement("h1", {}, " this is basically div > (div > h1*2)*2")
+                        ])
+                ,
+                React.createElement("div",
+                        { id: "c2" },
+                        [
+                                React.createElement("h1", {}, " this is basically div > (div > h1*2)*2"), 
+                                React.createElement("h1", {}, " this is basically div > (div > h1*2)*2")
+                        ])
+        ]
+        
+);
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+root3.render(heading3);
